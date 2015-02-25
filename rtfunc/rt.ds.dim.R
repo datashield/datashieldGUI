c676fb05aa98a8b2d627669411f9c6decc05e908
Andrew Turner <andrewjamesturner0@gmail.com>
2015-02-25 16:46:10 +0000

    Change dim "type" option to a radio button
    Signed-off-by: Andrew Turner <andrewjamesturner0@gmail.com>

--- rtfunc/rt.ds.dim.R
+++ rtfunc/rt.ds.dim.R
@@ -5,7 +5,7 @@ rt.ds.dim = function(){
 	ex.ds.dim = function(){
 		tkdestroy(window.ds.dim)
 		x <- toString(tclvalue(xinput_))
-		type <- toString(tclvalue(typeinput_))
+		type <- toString(tclvalue(rbvalue))
 		print(eval(parse(text=   paste0("ds.dim('", x ,"','", type ,"')" , sep="")   )))
 	}
 
@@ -20,15 +20,21 @@ rt.ds.dim = function(){
 	inputframe0 = tkframe(window.ds.dim)
 
 	xinput_ = tclVar( 'D' )
-	typeinput_ = tclVar( 'combine' )
+	rbvalue <- tclVar( 'combine' )
 
 	inputframe1 = tkframe(window.ds.dim)
 	tkpack(tklabel(inputframe1,text='Dataframe', width=20),side='left')
 	tkpack(tkentry(inputframe1,width=30,textvariable=xinput_), side='left', pady=c(2,2), padx=c(2,5))
 
 	inputframe2 = tkframe(window.ds.dim)
-	tkpack(tklabel(inputframe2,text='Type', width=20),side='left')
-	tkpack(tkentry(inputframe2,width=30,textvariable=typeinput_), side='left', pady=c(2,2), padx=c(2,5))
+        rbsplt <- tkradiobutton(inputframe2)
+        rbcomb <- tkradiobutton(inputframe2)
+        tkconfigure(rbsplt, variable=rbvalue, value="split")
+        tkconfigure(rbcomb, variable=rbvalue, value="combine")
+	tkpack(tklabel(inputframe2,text='Split', width=10), side='left', pady=c(5,5), padx=c(10,5))
+        tkpack(rbsplt,side='left', pady=c(5,5), padx=c(5,5))
+	tkpack(tklabel(inputframe2,text='Combine', width=10), side='left', pady=c(5,5), padx=c(5,5))
+        tkpack(rbcomb,side='left', pady=c(5,5), padx=c(5,10))
 
 	tkpack(tkbutton(inputframe0,text='Execute',command=ex.ds.dim),side='left', pady=c(10,10) , padx=c(10,5))
 	tkpack(tkbutton(inputframe0,text='Cancel',command=cl.ds.dim),side='left', pady=c(10,10) , padx=c(5,10))
