0e1beb1e0e1442bee13efc1eb68d845b89b9b661
Andrew Turner <andrewjamesturner0@gmail.com>
2015-03-09 13:55:58 +0000

    Add ability to run ds.asMatrix
    Signed-off-by: Andrew Turner <andrewjamesturner0@gmail.com>

--- realTime.R
+++ realTime.R
@@ -232,6 +232,7 @@ functionSelection = function(){
 	tkpack(tkbutton(assignframe2,text='ds.asCharacter',command=rt.ds.asCharacter),side='left', pady=c(10,10) , padx=c(10,5))
 	tkpack(tkbutton(assignframe2,text='ds.asFactor',command=rt.ds.asFactor),side='left', pady=c(10,10) , padx=c(5,5))
 	tkpack(tkbutton(assignframe2,text='ds.asList',command=rt.ds.asList),side='left', pady=c(10,10) , padx=c(5,5))
+	tkpack(tkbutton(assignframe2,text='ds.asMatrix',command=rt.ds.asMatrix),side='left', pady=c(10,10) , padx=c(5,5))
 
 	tkpack(assignframe)
 	tkpack(assignframe0)
