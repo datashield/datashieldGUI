bb030a1af14673703a7aee20eb3ea80a5b7d1536
Andrew Turner <andrewjamesturner0@gmail.com>
2015-03-10 12:58:00 +0000

    Add ability to rub ds.cor
    Signed-off-by: Andrew Turner <andrewjamesturner0@gmail.com>

---
+++ rtfunc/rt.ds.cor.R
@@ -0,0 +1,40 @@
+# Real time ds.cor function
+rt.ds.cor = function(){
+	
+	# Execute function
+	ex.ds.cor = function(){
+		tkdestroy(window.ds.cor)
+		x <- toString(tclvalue(xinput_))
+		y <- toString(tclvalue(yinput_))
+		print(eval(parse(text=   paste0("ds.cor('", x , "','" , y , "')" , sep="")   )))
+	}
+
+	# Cancel function
+	cl.ds.cor = function(){
+		tkdestroy(window.ds.cor)
+	}
+
+	# Draw input window
+	window.ds.cor = tktoplevel()
+	tkwm.title(window.ds.cor,'ds.cor()')
+	inputframe0 = tkframe(window.ds.cor)
+
+	xinput_ = tclVar( 'D$LAB_HDL' )
+	yinput_ = tclVar( 'D$LAB_TSC' )
+
+	inputframe1 = tkframe(window.ds.cor)
+	tkpack(tklabel(inputframe1,text='X', width=20),side='left')
+	tkpack(tkentry(inputframe1,width=30,textvariable=xinput_), side='left', pady=c(2,2), padx=c(2,5))
+
+	inputframe2 = tkframe(window.ds.cor)
+	tkpack(tklabel(inputframe2,text='Y', width=20),side='left')
+	tkpack(tkentry(inputframe2,width=30,textvariable=yinput_), side='left', pady=c(2,2), padx=c(2,5))
+
+	tkpack(tkbutton(inputframe0,text='Execute',command=ex.ds.cor),side='left', pady=c(10,10) , padx=c(10,5))
+	tkpack(tkbutton(inputframe0,text='Cancel',command=cl.ds.cor),side='left', pady=c(10,10) , padx=c(5,10))
+
+	tkpack(inputframe1)
+	tkpack(inputframe2)
+	tkpack(inputframe0)
+}
+
